a2793500c7b11273d505cde82306a5e448187b6a
GenaKashin <gengambit37@gmail.com>
2020-08-09 17:52:18 +0300

    Created Layout.css

--- react-quiz/src/hoc/Layout/Layout.js
+++ react-quiz/src/hoc/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react'
+import classes from './Layout.css'
 
  export default class Layout extends Component {
     render() {
         return (
-            <div>
+            <div className={classes.Layout}>
                 {this.props.children}
             </div>
         )
